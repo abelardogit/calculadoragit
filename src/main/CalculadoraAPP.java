b38777572f2f633ab45c49281b7790fd432e0fac
Abelardo León González <github@abelardolg.com>
2025-02-03 11:18:35 +0100

    Ready to welcome

--- src/main/CalculadoraAPP.java
+++ src/main/CalculadoraAPP.java
@@ -2,6 +2,6 @@ package main;
 
 public class CalculadoraAPP {
     public static void main (String[] args) {
-        System.out.println("Hola, mundo");
+        System.out.println("Welcome to CalculadoraGIT!");
     }
 }
